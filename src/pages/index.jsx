b874d4296fb61132d367f3791a4d7731df420588
Mickael Turquais <mickael.turquais@laposte.net>
2024-05-17 11:56:55 +0200

    ajout meta

---
+++ src/pages/index.jsx
@@ -0,0 +1,76 @@
+import React, {useState} from "react";
+import Head from "next/head";
+
+import LayoutDefault from "@/containers/layout";
+import PopUp from "@/components/popUp";
+
+import { Main } from "@/assets/styles/main.styled";
+
+export default function Home() {
+  const [isVisiblePopUp, setIsVisiblePopUp] = useState (true)
+
+  const closePopUp = () => {
+      setIsVisiblePopUp(false);
+  };
+  return (
+    <>
+      <Head>
+        <title>Nanou Services</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Des services à domicile qui simplifient votre vie - Garde d'enfants / Cuisine Maison / Femme de ménage / Conciergerie" />
+        <meta name="keywords" content="services à domicile, garde d'enfants, cuisine maison, femme de ménage, conciergerie, Nivillac, La Roche-Bernard, Herbignac, Pénestin, Muzillac, Morbihan" />
+        <meta name="author" content="Nanou Services" />
+        <meta property="og:title" content="Nanou Services - Services à domicile" />
+        <meta
+          property="og:description"
+          content="Des services à domicile qui simplifient votre vie - Garde d'enfants, Cuisine Maison, Femme de ménage, Conciergerie"
+        />
+         <script type="application/ld+json">
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": "LocalBusiness",
+            "name": "Nanou Services",
+            "description": "Des services à domicile qui simplifient votre vie - Garde d'enfants, Cuisine Maison, Femme de ménage, Conciergerie",
+            "url": "https://www.nanou-services.fr",
+            "logo": "https://www.nanou-services.fr/assets/images/logo Nanou.png",
+            "address": [
+              {
+                "@type": "PostalAddress",
+                "addressLocality": "Nivillac",
+                "addressCountry": "FR"
+              },
+              {
+                "@type": "PostalAddress",
+                "addressLocality": "La Roche-Bernard",
+                "addressCountry": "FR"
+              },
+              {
+                "@type": "PostalAddress",
+                "addressLocality": "Herbignac",
+                "addressCountry": "FR"
+              },
+              {
+                "@type": "PostalAddress",
+                "addressLocality": "Muzillac",
+                "addressCountry": "FR"
+              },
+              {
+                "@type": "PostalAddress",
+                "addressLocality": "Pénestin",
+                "addressCountry": "FR"
+              }
+            ],
+          }
+          `}
+        </script>
+        
+      </Head>
+      <LayoutDefault>
+        <Main>
+          <PopUp isVisible={isVisiblePopUp} onClose={closePopUp}>Site internet en cours de développement</PopUp>
+        </Main>
+      </LayoutDefault>
+    </>
+  );
+}
